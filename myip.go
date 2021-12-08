d9e725a2f2d42213652b2a9e8216117eb6040670
OpalineM28 <opaline.martin@epsi.fr>
2021-12-08 15:08:06 +0100

    tp1-Packer

---
+++ myip.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://api.ipify.org?format=text"
+	fmt.Printf("Getting IP address from  ipify\n")
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("My IP is:%s\n", ip)
+}
---
+++ script.sh
@@ -0,0 +1,1 @@
+go build myip.go
